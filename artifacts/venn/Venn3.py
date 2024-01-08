5f6bbb47b68b1fd0fde4fe9fadd37afb041b7f91
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-01-08 10:04:02 +0100

    save fig in plot

--- artifacts/venn/Venn3.py
+++ artifacts/venn/Venn3.py
@@ -5,10 +5,16 @@ from matplotlib_venn import venn3
 
 # Can only do up to 3 sets but calculates real overlaps of the venn diagrams.
 # Upset plots for more?
-tim = {"Event-Scheduling coordination", "Dedicated coordination elements", "Predefined", "Timed:Logical", "Formal language", "Heterogeneous", "Non-Intrusive", "Discrete event system", "Coordinated simulation", "Custom properties"}
-wright = {"Data-driven coordination", "Architecture based", "Customizable", "Timed:Logical", "Causal", "Formal language", "Homogeneous", "Intrusive", "Discrete event system", "Coordinated simulation", "Model checking"}
-linda = {"Data-driven coordination", "Name based", "Predefined", "Causal", "Programming language", "Homogeneous", "Intrusive", "Discrete event system", "Coordinated execution"}
+tim = {"Event-Scheduling coordination", "Dedicated coordination elements", "Predefined", "Timed:Logical",
+       "Formal language", "Heterogeneous", "Non-Intrusive", "Discrete event system", "Coordinated simulation",
+       "Custom properties"}
+wright = {"Data-driven coordination", "Architecture based", "Customizable", "Timed:Logical", "Causal",
+          "Formal language", "Homogeneous", "Intrusive", "Discrete event system", "Coordinated simulation",
+          "Model checking"}
+linda = {"Data-driven coordination", "Name based", "Predefined", "Causal", "Programming language", "Homogeneous",
+         "Intrusive", "Discrete event system", "Coordinated execution"}
 
 venn3([tim, wright, linda], ("BCoordLang", "Wright", "Linda"))
 
+# plt.savefig("venn3.svg", format="svg", bbox_inches="tight")
 plt.show()
