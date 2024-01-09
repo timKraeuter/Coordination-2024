feaf9126454de341ebe4d9b562a3fe9488d910d3
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-01-09 14:16:11 +0100

    Handle cells with multiple features

--- artifacts/venn/Venn_3.py
+++ artifacts/venn/Venn_3.py
@@ -10,10 +10,10 @@ import pandas as pd
 approach_names = sys.argv[1:]
 
 if len(sys.argv) != 4:  # Includes the script name
-  print(
-      'There should be exactly 3 arguments to select 3 coordination approaches! Current arguments:',
-      str(approach_names))
-  sys.exit(1)
+    print(
+        'There should be exactly 3 arguments to select 3 coordination approaches! Current arguments:',
+        str(approach_names))
+    sys.exit(1)
 
 df = pd.read_excel('../classification.xlsx')
 # Lower case to find things more conveniently.
@@ -21,14 +21,23 @@ df.columns = map(str.lower, df.columns)
 
 approach_features = []
 for approach in approach_names:
-  approach = approach.lower()
-  # Filter nan, to lowercase, to array.
-  features = df[approach][df[approach].notnull()].str.lower().values
-  approach_features.append(set(features))
+    approach = approach.lower()
+    # Filter nan, to lowercase, to array.
+    feature_cells = df[approach][df[approach].notnull()].str.lower().values
+    feature_set = set()
+    for feature_cell in feature_cells:
+        # Handle cells that contain multiple features separated by ,
+        if feature_cell.find(",") != -1:
+            for feature in feature_cell.split(","):
+                feature = feature.strip()
+                feature_set.add(feature)
+        else:
+            feature_set.add(feature_cell)
+    approach_features.append(feature_set)
 
 venn3(approach_features, approach_names)
 
 filename = '_'.join(approach_names) + "_venn"
 
-plt.savefig(filename + ".svg", format="svg", bbox_inches="tight")
+# plt.savefig(filename + ".svg", format="svg", bbox_inches="tight")
 plt.show()
