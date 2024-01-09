a158154319aa0b9afab9f9a7bd48a5f42e9685aa
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-01-09 15:01:19 +0100

    Add readme

---
+++ artifacts/venn/README.md
@@ -0,0 +1,6 @@
+Make a venn diagram for three chosen approaches:
+```bash
+py Venn_3.py BCoordLang BCOol Ptolemy
+```
+
+See sample output diagram in `./BCoordLang_BCOol_Ptolemy_venn.svg` .
--- artifacts/venn/Venn_3.py
+++ artifacts/venn/Venn_3.py
@@ -41,7 +41,6 @@ venn3(approach_features, approach_names)
 filename = '_'.join(approach_names) + "_venn"
 
 plt.savefig(filename + ".svg", format="svg", bbox_inches="tight")
-plt.show()
 
 # Print diff and overlap statistics.
 A = approach_features[0]
@@ -56,13 +55,15 @@ print("{}'s unique features ({}): {}".format(A_name, len(A - B - C), str(A - B -
 print(
     "{}'s and {}'s common features ({}) without {}: {}".format(A_name, B_name, len(A & B - C), C_name, str(A & B - C)))
 print(
-    "{}'s and {}'s common features ({}) without {}: {}".format(A_name, C_name, len(A & C - B), B_name, str(A & C - B)))
+    "{}'s and {}'s common features ({}) without {}: {}\n".format(A_name, C_name, len(A & C - B), B_name, str(A & C - B)))
 
 # Do B
 print("{}'s unique features ({}): {}".format(B_name, len(B - A - C), str(B - A - C)))
 print(
-    "{}'s and {}'s common features ({}) without {}: {}".format(B_name, C_name, len(B & C - A), A_name, str(B & C - A)))
+    "{}'s and {}'s common features ({}) without {}: {}\n".format(B_name, C_name, len(B & C - A), A_name, str(B & C - A)))
 
 # Do C
 print("{}'s unique features ({}): {}".format(C_name, len(C - A - B), str(C - A - B)))
 print("Common features ({}): {}".format(len(A & B & C), str(A & B & C)))
+
+plt.show()
