e4155ca3ed3b503386fa70bcb5373622515d607b
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-01-09 14:57:18 +0100

    print diff and overlap stats

--- artifacts/venn/Venn_3.py
+++ artifacts/venn/Venn_3.py
@@ -2,6 +2,7 @@
 
 # venn3 can only do a venn diagram of up to 3 sets.
 
+import itertools
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 import sys
@@ -39,5 +40,29 @@ venn3(approach_features, approach_names)
 
 filename = '_'.join(approach_names) + "_venn"
 
-# plt.savefig(filename + ".svg", format="svg", bbox_inches="tight")
+plt.savefig(filename + ".svg", format="svg", bbox_inches="tight")
 plt.show()
+
+# Print diff and overlap statistics.
+A = approach_features[0]
+B = approach_features[1]
+C = approach_features[2]
+A_name = approach_names[0]
+B_name = approach_names[1]
+C_name = approach_names[2]
+
+# Do A
+print("{}'s unique features ({}): {}".format(A_name, len(A - B - C), str(A - B - C)))
+print(
+    "{}'s and {}'s common features ({}) without {}: {}".format(A_name, B_name, len(A & B - C), C_name, str(A & B - C)))
+print(
+    "{}'s and {}'s common features ({}) without {}: {}".format(A_name, C_name, len(A & C - B), B_name, str(A & C - B)))
+
+# Do B
+print("{}'s unique features ({}): {}".format(B_name, len(B - A - C), str(B - A - C)))
+print(
+    "{}'s and {}'s common features ({}) without {}: {}".format(B_name, C_name, len(B & C - A), A_name, str(B & C - A)))
+
+# Do C
+print("{}'s unique features ({}): {}".format(C_name, len(C - A - B), str(C - A - B)))
+print("Common features ({}): {}".format(len(A & B & C), str(A & B & C)))
