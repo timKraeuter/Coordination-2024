63c78c7727b9cc4264a4cfe46b00d98f4d9d0ea7
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-01-09 11:59:35 +0100

    writing venn script

---
+++ artifacts/venn/Venn_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# venn3 can only do a venn diagram of up to 3 sets.
+
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn3
+import sys
+import pandas as pd
+
+args = sys.argv[1:]
+
+if len(sys.argv) != 4:  # Includes the script name
+  print(
+      'There should be exactly 3 arguments to select 3 coordination approaches! Current arguments:',
+      str(args))
+  sys.exit(1)
+
+df = pd.read_excel('../classification.xlsx')
+
+approaches = []
+for approach in args:
+  print(approach)
+  # Filter nan, to lowercase, to array.
+  values = df[approach][df[approach].notnull()].str.lower().values
+  approaches.append(set(values))
+
+print(approaches)
+
+venn3(approaches, args)
+
+# plt.savefig("coordination_venn3.svg", format="svg", bbox_inches="tight")
+plt.show()
